e69964eb62cd7b5b338511ff4adb3f56b919b44d
Hasan Tanich <hasantanich@gmail.com>
2022-08-01 13:14:56 +0200

    admin login screen added

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -5,12 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
+
 export class AdminComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("xd");
-  }
 
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { AppComponent } from './app.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: 'homepage', component: AppComponent },
-  { path: 'admin', component: AdminComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'homepage', component: DashboardComponent },
+  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
